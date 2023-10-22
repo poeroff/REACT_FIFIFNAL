7499543d193f684556fc1d9789761dd586a09d4b
sunah <1004_sunah@naver.com>
2023-10-23 02:19:40 +0900

    a태그 수정

--- src/mypageComponents/MypageRoot.js
+++ src/mypageComponents/MypageRoot.js
@@ -7,23 +7,47 @@ const MypageRoot = () => {
                 <nav className={classes.nav}>
                     <span className={classes.nav_span}>바로접속 ON</span>
                     <div className={classes.nav_div}>
-                        <h3>나의 쇼핑 활동</h3>
-                        <a>주문 내역 조회</a>
-                        <a>구매후기</a>
-                        <a>래플 응모내역</a>
-                        <a>상품문의</a>
-                        <a>1:1문의</a>
-                        <a>최근 본 상품</a>
-                        <a>좋아요</a>
-                        <a>나의 맞춤 정보</a>
-                        <a>무신사페이 관리</a>
-                        <a>무신사 현대카드 보기</a>
-                        <a>매장 결제 내역</a>
-                        <a>회원 혜택</a>
+                        <p>나의 쇼핑 활동</p>
+                        <button type="button">
+                            <span> 주문내역조회</span>
+                        </button>
+                        <button type="button">
+                            <span> 구매후기</span>
+                        </button>
+                        <button type="button">
+                            <span> 래플 응모내역</span>
+                        </button>
+                        <button type="button">
+                            <span> 상품문의</span>
+                        </button>
+                        <button type="button">
+                            <span> 1:1문의</span>
+                        </button>
+                        <button type="button">
+                            <span> 최근 본 상품</span>
+                        </button>
+                        <button type="button">
+                            <span> 좋아요</span>
+                        </button>
+                        <button type="button">
+                            <span> 나의 맞춤 정보</span>
+                        </button>
+                        <button type="button">
+                            <span> 무신사페이 관리</span>
+                        </button>
+                        <button type="button">
+                            <span> 무신사 현대카드 보기</span>
+                        </button>
+                        <button type="button">
+                            <span> 매장 결제 내역</span>
+                        </button>
+                        <button type="button">
+                            <span> 회원 혜택</span>
+                        </button>
                     </div>
                     <div className={classes.nav_div}>
-                        <h3>커뮤니티</h3>
-                        {/* <a>게시물/스크랩/댓글</a> */}
+                        <p>커뮤니티</p>
+                        {}
                     </div>
                 </nav>
             </div>
